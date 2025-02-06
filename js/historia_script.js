63fc1b4e525e62df2befc5a322ae42216212cabe
DARR-1 <a01665821@tec.mx>
2025-02-05 23:04:12 -0600

    Actualizar script y estilos de historia, añadiendo enlaces de galería y ajustando posiciones

--- js/historia_script.js
+++ js/historia_script.js
@@ -6,7 +6,7 @@ function scrollToSection(id) {
     document.getElementById(lastId + "-button").style.color = "rgba(255, 255, 0, 0.658)";
     document.getElementById(lastId + "-button").style.fontSize = "2.5vh";
     document.getElementById(id).scrollIntoView({ behavior: 'smooth' });
-    document.getElementById("pointer").style.top = 16.5 + (id - 2012) * 5.45 + "vh";
+    document.getElementById("pointer").style.top = 16.5 + (id - 2012) * 5.48 + "vh";
     document.getElementById(id + "-button").style.color = "yellow";
     document.getElementById(id + "-button").style.fontSize = "3.5vh";
     lastId = id;
