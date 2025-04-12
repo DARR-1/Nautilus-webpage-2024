c3c0dc182ce23239c96d4218fe71e239f711bb5f
André <151085951+DARR-1@users.noreply.github.com>
2025-04-11 23:29:53 -0600

    Update script_index.js

--- js/script_index.js
+++ js/script_index.js
@@ -3,14 +3,14 @@ var clicked = false;
 document.getElementById("menu-button").onclick = function () {
     if (!clicked) {
         document.getElementById("menu-button").style.transform = "translate(-50vw, 0) rotate(0)";
-        document.getElementById("menu-button").style.backgroundImage = "url('Nautilus-webpage-2024/Assets/images/Iconos/cerrar.png')";
+        document.getElementById("menu-button").style.backgroundImage = "url('Assets/images/Iconos/cerrar.png')";
         clicked = true;
         console.log("clickearon el menu");
         console.log(clicked);
         document.body.style.overflowY = 'hidden';
     } else {
         document.getElementById("menu-button").style.transform = "translate(0, 0) rotate(0)";
-        document.getElementById("menu-button").style.backgroundImage = "url('Nautilus-webpage-2024/Assets/images/Iconos/menu.png')";
+        document.getElementById("menu-button").style.backgroundImage = "url('Assets/images/Iconos/menu.png')";
         clicked = false;
         console.log("clickearon el menu pero con false");
         console.log(clicked);
